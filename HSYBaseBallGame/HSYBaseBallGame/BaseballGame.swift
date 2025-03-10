2764cc98970a44477285636828e8f0d1fae8fba0
seoyounghan <happyunbd364@naver.com>
2025-03-10 17:14:30 +0900

    feat: [#1] 정답 만들기 로직 추가 -> 세 숫자가 모두 다르도록 변경

--- HSYBaseBallGame/HSYBaseBallGame/BaseballGame.swift
+++ HSYBaseBallGame/HSYBaseBallGame/BaseballGame.swift
@@ -7,13 +7,36 @@
 import Foundation
 
 class BaseballGame {
-    let randomBound: ClosedRange<Int> = 100...999
-    
     func start() {
         let answer = makeAnswer()
+        print(answer)
     }
 
     func makeAnswer() -> Int {
-        return Int.random(in: randomBound)
+        var allNumbers: [Int] = Array(1...9)
+        var answerNumber: Int = 0
+
+        if let firstNumber = allNumbers.randomElement() {
+            allNumbers.remove(at: allNumbers.firstIndex(of: firstNumber)!)
+            answerNumber += firstNumber * 100
+        } else {
+            print("정답첫번째 숫자 오류")
+        }
+
+        if let secondNumber = allNumbers.randomElement() {
+            allNumbers.remove(at: allNumbers.firstIndex(of: secondNumber)!)
+            answerNumber += secondNumber * 10
+        } else {
+            print("정답두번째 숫자 오류")
+        }
+
+        if let thirdNumber = allNumbers.randomElement() {
+            allNumbers.remove(at: allNumbers.firstIndex(of: thirdNumber)!)
+            answerNumber += thirdNumber
+        } else {
+            print("정답세번째 숫자 오류")
+        }
+
+        return answerNumber
     }
 }
