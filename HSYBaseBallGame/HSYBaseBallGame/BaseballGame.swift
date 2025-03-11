cb33ef90594c3b0083f880a05e26ebedc43b3249
seoyounghan <happyunbd364@naver.com>
2025-03-11 13:08:50 +0900

    feat: [#3] - 사용자에게 입력 받고 입력을 처리하는 기능 추가

--- HSYBaseBallGame/HSYBaseBallGame/BaseballGame.swift
+++ HSYBaseBallGame/HSYBaseBallGame/BaseballGame.swift
@@ -8,9 +8,18 @@ import Foundation
 
 class BaseballGame {
     func start() {
+        print("< 게임을 시작합니다 >")
         let answer = makeAnswer()
-        print(answer)
+        //print(answer)
+        var isCorrect: Bool = false
+
+        while !isCorrect {
+            isCorrect = checkInput(input: makeInput(),
+                                   answer: answer)
+        }
     }
+
+    // MARK: - 정답 생성 함수
 
     func makeAnswer() -> Int {
         var allNumbers = Array(1...9)
@@ -22,6 +31,8 @@ class BaseballGame {
 
         return answer
     }
+
+    // MARK: - 랜덤 숫자 생성 함수
 
     func makeNumber(from numbers: inout [Int], multiplier: Int) -> Int {
         if let number = numbers.randomElement() {
@@ -35,4 +46,71 @@ class BaseballGame {
             return 0
         }
     }
+
+    // MARK: - 입력 받는 함수
+
+    func makeInput() -> String? {
+        print("숫자를 입력하세요")
+        let inputString = readLine()
+
+        return inputString
+    }
+
+    // MARK: - 입력 처리 함수
+
+    func checkInput(input: String?, answer: Int) -> Bool {
+        guard let unwrappedInput = input else {
+            print("올바르지 않은 입력값입니다.")
+            return false
+        }
+
+        guard let inputNumber = Int(unwrappedInput) else {
+            print("올바르지 않은 입력값입니다.")
+            return false
+        }
+
+        if unwrappedInput.count != 3 {
+            print("올바르지 않은 입력값입니다.")
+            return false
+        }
+
+        if inputNumber == answer {
+            print("정답입니다!")
+            return true
+        }
+
+        let (strike, ball) = compareDigits(input: inputNumber, answer: String(answer))
+
+        if strike == 0 && ball == 0 {
+            print("Nothing")
+        } else if strike <= 0 {
+            print("\(ball)볼")
+        } else if ball <= 0 {
+            print("\(strike)스트라이크")
+        } else {
+            print("\(strike)스트라이크 \(ball)볼")
+        }
+
+        return false
+    }
+
+    // MARK: - 볼/스트라이크 갯수 확인 함수
+
+    func compareDigits(input: Int, answer: String) -> (strike: Int, ball: Int) {
+        var strikeCount = 0
+        var ballCount = 0
+
+        let inputString = String(input)
+
+        for (index, char) in inputString.enumerated() {
+            if char == answer[answer.index(answer.startIndex, offsetBy: index)] {
+                strikeCount += 1
+            } else if answer.contains(char) {
+                ballCount += 1
+            }
+        }
+
+        return (strikeCount, ballCount)
+    }
+
 }
